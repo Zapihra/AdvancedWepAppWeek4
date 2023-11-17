0f845474e2dc7b7b184c4f892a44891ccb8de3c3
Zapihra <iida.vaaja@student.lut.fi>
2023-11-17 16:46:54 +0200

    task2 show api

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,6 +1,12 @@
 
 
-window.onload.fetch("http://localhost/recipe/pizza").then(function (res) {return res.json();
+
+
+fetch("http://localhost:3000/recipe/Lasagna").then(function (res) {return res.json();
 }).then(function (data) {
-    console.log(data)
+    const body = document.getElementById("body");
+    const p = document.createElement("p")
+    p.textContent = data
+    body.appendChild(p)
 })
+
