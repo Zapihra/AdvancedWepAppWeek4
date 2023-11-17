099f0999d6237f982b0a8a22e50933baaed91f93
Zapihra <iida.vaaja@student.lut.fi>
2023-11-17 18:27:11 +0200

    task3 adding recipe to api

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,11 +1,7 @@
-
-
-
-
 fetch("http://localhost:3000/recipe/Pizza")
 .then(function (res) {return res.json();
 }).then(function (data) {
-    console.log(data)
+
     const body = document.getElementById("body");
     const p1 = document.createElement("p")
     const p2 = document.createElement("p")
@@ -19,3 +15,57 @@ fetch("http://localhost:3000/recipe/Pizza")
     body.appendChild(p3)
 })
 
+
+const ingButton = document.getElementById("add-ingredient")
+const instButton = document.getElementById("add-instruction")
+const submitButton = document.getElementById("submit")
+let ingredientList = []
+let instructionList = []
+
+ingButton.addEventListener("click", () => {
+    const text = document.getElementById("ingredients-text").value
+    ingredientList.push(text)
+    console.log(ingredientList)
+})
+
+instButton.addEventListener("click", () => {
+    const text = document.getElementById("instructions-text").value
+    instructionList.push(text)
+    console.log(instructionList)
+})
+
+submitButton.addEventListener("click" , () => {
+    const name = document.getElementById("name-text").value;
+
+    fetch("http://localhost:3000/recipe/", {
+        method: 'POST',
+        body: JSON.stringify({
+            "name": name,
+            "instructions": instructionList,
+            "ingredients": ingredientList
+        }),
+        headers: {
+            'Content-Type':'application/json'
+        }
+    }).then(function (res) {return res.json();
+    }).then(function (data) {
+
+        console.log(data)
+
+        const body = document.getElementById("body");
+        const p1 = document.createElement("p")
+        const p2 = document.createElement("p")
+        const p3 = document.createElement("p")
+        p1.textContent = data.name
+        p2.textContent = data.ingredients
+        p3.textContent = data.instructions
+
+        body.appendChild(p1)
+        body.appendChild(p2)
+        body.appendChild(p3)
+    })
+    ingredientList = []
+    instructionList = []
+
+
+})
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+const record = [];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
@@ -13,6 +15,11 @@ router.get('/recipe/:food', function(req,res) {
   "instructions": ["list"], 
   "ingredients": ["list2"]}
   res.json(resText);
+})
+
+router.post('/recipe/', function(req, res) {
+  record.push(req.body)
+  res.json(req.body)
 })
 
 module.exports = router;
