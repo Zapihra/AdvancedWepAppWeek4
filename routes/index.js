a80fc517bdfefd2f6a24be630ca9b33ff37bc08f
Zapihra <iida.vaaja@student.lut.fi>
2023-11-17 14:18:25 +0200

    express template and task1 API

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/recipe/:food', function(req,res) {
+  const food = req.params.food;
+  resText = {"name": food, 
+  "instructions": ["list"], 
+  "ingredients": ["list2"]}
+  res.json(JSON.stringify(resText));
+})
+
+module.exports = router;
